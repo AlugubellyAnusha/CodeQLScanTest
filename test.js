fad3cb7966eaa2311e9f8a33d2d3548739c6b69b
AlugubellyAnusha <alugubellyanusha@gmail.com>
2025-02-14 12:39:21 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,19 +1,6 @@
-<!DOCTYPE html>
-<html>
-<body>
+function test(testString){
+  return new RegExp(testString.replace(/\$/g,"\\$"));
+  
 
-<h1>JavaScript Strings</h1>
-<h2>The replace() Method</h2>
-
-<p>replace() searches a string for a value,
-and returns a new string with the specified value(s) replaced:</p>
-
-<p id="demo">Visit Microsoft!</p>
-
-<script>
-let text = document.getElementById("demo").innerHTML; 
-document.getElementById("demo").innerHTML = text.replace("Microsoft", "W3Schools");
-</script>
-
-</body>
-</html>
+  
+}
